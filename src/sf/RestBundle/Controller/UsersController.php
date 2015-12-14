ac1975c2b15a5e8fa926e40eb77b2f39d669527c
otgonoo <p.otgonbayar@gmail.com>
2015-12-14 18:01:24 +0800

    first try

---
+++ src/sf/RestBundle/Controller/UsersController.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UsersController
+ *
+ * @author otgonoo
+ */
+
+namespace sf\RestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations\View;
+
+class UsersController extends Controller
+{
+
+    /**
+     * @return array
+     * @view()
+     */
+    public function getUsersAction()
+    {
+        $users = $this->getDoctrine()->getRepository('sfRestBundle:User')
+                ->findAll();
+        return array('users' => $users);
+    }
+
+}
