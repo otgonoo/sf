ae0039a0517887ad69d75680fac7e12ba5749a54
otgonoo <p.otgonbayar@gmail.com>
2015-12-14 17:21:07 +0800

    init 2.8

---
+++ README.md
@@ -0,0 +1,4 @@
+sf
+==
+
+A Symfony project created on December 14, 2015, 5:10 pm.
---
+++ src/sf/RestBundle/sfRestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace sf\RestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class sfRestBundle extends Bundle
+{
+}
